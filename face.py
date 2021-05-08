b6051284fb106cd023bfefddc82346d37ce2c561
keshav-pc <63454393+keshav-pc@users.noreply.github.com>
2021-05-08 12:26:31 +0530

    Update face.py

--- face.py
+++ face.py
@@ -1,4 +1,4 @@
-# this is program for face detection and age and gender dectection
+# this is program for face detection and age dectection
 import cv2
 import math
 import argparse
@@ -32,16 +32,12 @@ faceProto = "opencv_face_detector.pbtxt"
 faceModel = "opencv_face_detector_uint8.pb"
 ageProto = "age_deploy.prototxt"
 ageModel = "age_net.caffemodel"
-genderProto = "gender_deploy.prototxt"
-genderModel = "gender_net.caffemodel"
 
 MODEL_MEAN_VALUES = (78.4263377603, 87.7689143744, 114.895847746)
 ageList = ['(0-2)', '(4-6)', '(8-12)', '(15-20)', '(25-32)', '(38-43)', '(48-53)', '(60-100)']
-genderList = ['Male', 'Female']
 
 faceNet = cv2.dnn.readNet(faceModel, faceProto)
 ageNet = cv2.dnn.readNet(ageModel, ageProto)
-genderNet = cv2.dnn.readNet(genderModel, genderProto)
 
 video = cv2.VideoCapture(args.image if args.image else 0)
 padding = 20
@@ -61,16 +57,12 @@ while cv2.waitKey(1) < 0:
                                                                     :min(faceBox[2] + padding, frame.shape[1] - 1)]
 
         blob = cv2.dnn.blobFromImage(face, 1.0, (227, 227), MODEL_MEAN_VALUES, swapRB=False)
-        genderNet.setInput(blob)
-        genderPreds = genderNet.forward()
-        gender = genderList[genderPreds[0].argmax()]
-        print(f'Gender: {gender}')
-
+       
         ageNet.setInput(blob)
         agePreds = ageNet.forward()
         age = ageList[agePreds[0].argmax()]
         print(f'Age: {age[1:-1]} years')
 
-        cv2.putText(resultImg, f'{gender}, {age}', (faceBox[0], faceBox[1] - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.8,
+        cv2.putText(resultImg, f'{age}', (faceBox[0], faceBox[1] - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.8,
                     (0, 255, 255), 2, cv2.LINE_AA)
         cv2.imshow("Detecting age and gender", resultImg)
